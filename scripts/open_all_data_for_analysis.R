defc857895eca018ea589b2f1b8eebd007a7b3a4
jebyrnes <jarrett.byrnes@umb.edu>
2023-04-01 16:23:23 -0400

    Fixed biomass data issue

--- scripts/open_all_data_for_analysis.R
+++ scripts/open_all_data_for_analysis.R
@@ -8,8 +8,8 @@ totalabund<-read.csv("data/total_abund_clean.csv") %>%
 richness<-read.csv("data/sp_rich_clean.csv") %>% 
   filter(!is.na(VHedges.G )) 
 
-# biomass<-read.csv("data/biomass_clean.csv") %>% 
-#   filter(!is.na(VHedges.G )) 
+biomass<-read.csv("data/biomass_clean.csv") %>% 
+   filter(!is.na(VHedges.G )) 
 
 masterData <- read.csv("data/masterFish_clean.csv")%>% 
   filter(!is.na(VHedges.G )) %>% filter(Authors..Year.!="Steele et al. (2002)")
