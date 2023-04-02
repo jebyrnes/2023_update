7a17170922c7ee2bd29ac2bcea983c6610340c85
jebyrnes <jarrett.byrnes@umb.edu>
2023-04-02 13:43:04 -0400

    Adding richness and kelp morphology analyses

--- scripts/4_richness_analysis_figures.R
+++ scripts/4_richness_analysis_figures.R
@@ -1,5 +1,5 @@
 ############
-# Analyses and Figures of Total Abundance Effects
+# Analyses and Figures of Effects on Richness
 ############
 
 #Load libraries and Data
@@ -57,3 +57,64 @@ ggplot(sms3, aes(x=Type, y=estimate)) +
 
 ggsave("figures/Figure_4_TotalRichness_Type.jpg",
        dpi = 300)
+
+
+#### Richness and single v. multi for observational data
+rich_morphology_obs <- rma.mv(Hedges.G~ Single.or.Multi.Stipe+0, 
+       VHedges.G, 
+       random=list(~1|Authors..Year.),
+       data=richness %>% filter(Type.of.Study..Experimental_ObservatioNAl.=="Observational"),
+       control=list(optimizer="nlminb"))
+
+rich_morphology_exp <- rma.mv(Hedges.G~ Single.or.Multi.Stipe+0, 
+                              VHedges.G, 
+                              random=list(~1|Authors..Year.),
+                              data=richness %>% filter(Type.of.Study..Experimental_ObservatioNAl.=="Experimental"),
+                              control=list(optimizer="nlminb"))
+summary(rich_morphology_obs) # 72 studies - 21 multi, 51 single
+summary(rich_morphology_exp) # 14 studies - only 4 are single stipe
+
+#coefficient table
+rich_morph_coefs <- bind_rows(
+  coef(summary(rich_morphology_obs)) |>
+    mutate(`Type` = "Observational") |>
+    tibble::rownames_to_column(var = "Kelp Morphology"),
+  coef(summary(rich_morphology_exp)) |>
+    mutate(`Type` = "Experimental") |>
+    tibble::rownames_to_column(var = "Kelp Morphology")
+) |>
+  dplyr::rename(Hedges.G = estimate) |>
+  mutate(`Kelp Morphology` = gsub("Single.or.Multi.Stipe", "",
+                                  `Kelp Morphology`))
+
+rich_n <- richness %>% 
+  group_by(Type.of.Study..Experimental_ObservatioNAl., Single.or.Multi.Stipe) |>
+  dplyr::summarize(n = n(), .groups = "drop") |>
+  dplyr::rename(Type = Type.of.Study..Experimental_ObservatioNAl.,
+                `Kelp Morphology` = Single.or.Multi.Stipe)
+
+richness |>
+  dplyr::rename(Type = Type.of.Study..Experimental_ObservatioNAl.,
+                `Kelp Morphology` = Single.or.Multi.Stipe) |>
+ggplot(aes(y = Hedges.G, x = Type)) +
+  geom_jitter(alpha=0.7,
+              color = "grey",
+              position=position_jitter(width = .1), 
+              size=3) +
+  geom_point(data = rich_morph_coefs,
+             size=7, color="red") +
+  geom_linerange(data = rich_morph_coefs,
+                 mapping = aes(ymin=ci.lb, ymax=ci.ub),
+                 linewidth = 1) +
+  geom_text(data = rich_n,
+            aes(label = n),
+            y = -4,
+            size = 6) +
+  facet_wrap(vars(`Kelp Morphology`)) +
+  geom_hline(yintercept=0, lwd=1, lty=2) +
+  labs(x=NULL, y = "Hedge's G")
+
+ggsave("figures/Figure_X_Richness_Type_Morphology.jpg",
+       dpi = 300, 
+       width = 10)
+  
