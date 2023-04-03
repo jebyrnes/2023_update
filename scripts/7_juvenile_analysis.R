7cb094f57cb7da2357bb00c443f5ef802ef57f63
jebyrnes <jarrett.byrnes@umb.edu>
2023-04-03 18:40:33 -0400

    Cleaner juvenile analysis

--- scripts/7_juvenile_analysis.R
+++ scripts/7_juvenile_analysis.R
@@ -11,31 +11,33 @@ library(glue)
 library(broom)
 library(broom.mixed)
 library(ggplot2)
-library(wesanderson)
-theme_set(theme_classic(base_size = 20) )
+library(patchwork)
+theme_set(theme_classic(base_size = 24) )
 
 source("scripts/open_all_data_for_analysis.R")
+source("scripts/make_three_panel_plot.R")
+
 
 juveniles <- singleSpAbund |>
   filter(Juvenile..Adult..Both == "Juvenile")
 
 # Analysis of Study Type 
 juvHedges <- rma.mv(Hedges.G~ 0 + 
-                           Type.of.Study..Experimental_ObservatioNAl., 
-                         VHedges.G, 
-                         random=list(~1|Authors..Year., ~Authors..Year.|Site), 
-                         struct="HCS",rho=0,
-                         data=juveniles,
-                         verbose=TRUE, control=list(rel.tol=1e-8))
+                      Type.of.Study..Experimental_ObservatioNAl., 
+                    VHedges.G, 
+                    random=list(~1|Authors..Year., ~Authors..Year.|Site), 
+                    struct="HCS",rho=0,
+                    data=juveniles,
+                    verbose=TRUE, control=list(rel.tol=1e-8))
 tidy(juvHedges)
 
 # Analysis of Morphology
 juvMorphologyHedges <- rma.mv(Hedges.G~ 0 + 
-                      Single.or.Multi.Stipe, 
-                    VHedges.G, 
-                    random=list(~1|Authors..Year., ~Authors..Year.|Site), 
-                    struct="HCS",rho=0,
-                    data=juveniles)
+                                Single.or.Multi.Stipe, 
+                              VHedges.G, 
+                              random=list(~1|Authors..Year., ~Authors..Year.|Site), 
+                              struct="HCS",rho=0,
+                              data=juveniles)
 
 tidy(juvMorphologyHedges)
 
@@ -52,11 +54,11 @@ juvHedgesObs <- rma.mv(Hedges.G~ 0 +
 
 juvHedgesExp <- rma.mv(Hedges.G~ 0 + 
                          Single.or.Multi.Stipe, 
-                    VHedges.G, 
-                    random=list(~1|Authors..Year., ~Authors..Year.|Site), 
-                    struct="HCS",rho=0,
-                    data=juveniles |>
-                      filter(Type.of.Study..Experimental_ObservatioNAl. != "Observational"))
+                       VHedges.G, 
+                       random=list(~1|Authors..Year., ~Authors..Year.|Site), 
+                       struct="HCS",rho=0,
+                       data=juveniles |>
+                         filter(Type.of.Study..Experimental_ObservatioNAl. != "Observational"))
 # results
 tidy(juvHedgesObs) |>
   mutate(term =  gsub("Single.or.Multi.Stipe", "", term))
@@ -64,7 +66,31 @@ tidy(juvHedgesObs) |>
 tidy(juvHedgesExp)|>
   mutate(term =  gsub("Single.or.Multi.Stipe", "", term))
 
+### Make Figure 5 - 3 panel figure
+fig_5_panels <- get_model_figs(juvHedges,
+                               juvHedgesObs,
+                               juvHedgesExp,
+                               juveniles,
+                               ylim_vals = c(-5,5))
+
+### Combine fig panels
+make_3_panel_fig(fig_5_panels)
+
+ggsave(file = "figures/Figure_5_juveniles_type_morphology.jpg",
+       dpi=300,
+       width = 15)
+
+
+# check errant values
 juveniles |>
-  group_by(Type.of.Study..Experimental_ObservatioNAl., 
-           Single.or.Multi.Stipe) |>
-  summarize(n = n(), .groups = "drop")
+  filter(Hedges.G > 5) |>
+  select(3:4, Mean.kelp ,
+         Mean.no.Kelp,
+         sd_kelp,
+         sd_nokelp,
+         N..Kelp.,
+         N..no.kelp.,
+         Hedges.G) |> View()
+
+
+
